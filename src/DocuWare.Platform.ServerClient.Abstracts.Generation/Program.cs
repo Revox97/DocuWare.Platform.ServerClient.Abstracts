bed23aa83b770f7598f5e8101b88ed60228c5cd3
Leopold Pohlmann <Leopold.Pohlmann@gmx.net>
2025-12-07 10:01:18 +0100

    add: project files

---
+++ src/DocuWare.Platform.ServerClient.Abstracts.Generation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DocuWare.Platform.ServerClient.Abstracts.Generation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
